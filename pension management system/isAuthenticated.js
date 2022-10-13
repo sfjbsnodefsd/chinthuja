be0437d49a76634cc52959cf177658f57d21075d
sfjbssept <sfjbssept@gmail.com>
2022-10-13 17:27:41 +0000

    process pension updated

--- pension management system/isAuthenticated.js
+++ pension management system/isAuthenticated.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
-
-export async function isAuthenticated(req, res, next) {
+module.exports=async function isAuthenticated(req, res, next) {
   // Bearer <token>
   const token = req.headers["authorization"].split(" ")[1];
 
@@ -15,4 +14,4 @@ export async function isAuthenticated(req, res, next) {
       next();
     }
   });
-}
+};
